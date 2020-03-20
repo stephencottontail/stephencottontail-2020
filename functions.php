b91157d24f1bc8cff35ec4e52edfd9ca65fffac2
Stephen Dickinson <stephencottontail@me.com>
2020-03-20 12:22:26 -0600

    Bare minimum initial commit

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+add_action( 'genesis_setup', function() {
+    define( 'CHILD_THEME_NAME', 'stephencottontail-2020' );
+    define( 'CHILD_THEME_URL', 'https://stephencottontail.com/' );
+    define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+    add_theme_support( 'genesis-responsive-viewport' );
+    add_theme_support( 'genesis-accessibility', array( 'skip-links', 'headings' ) );
+    add_theme_support( 'genesis-footer-widgets', 2 );
+    add_theme_support( 'genesis-structural-wraps', array(
+	'footer',
+	'footer-widgets',
+	'header',
+	'nav',
+	'site-inner'
+    ) );
+    add_theme_support( 'genesis-menus', array(
+	'primary' => 'Primary Nav Menu',
+	'social' => 'Social Media Menu'
+    ) );
+        
+    add_theme_support( 'html5', array( 'caption', 'gallery', 'search-form' ) );
+
+    unregister_sidebar( 'header-right' );
+    unregister_sidebar( 'sidebar' );
+    unregister_sidebar( 'sidebar-alt' );
+
+    genesis_unregister_layout( 'content-sidebar' );
+    genesis_unregister_layout( 'sidebar-content' );
+    genesis_unregister_layout( 'content-sidebar-sidebar' );
+    genesis_unregister_layout( 'sidebar-content-sidebar' );
+    genesis_unregister_layout( 'sidebar-sidebar-content' );
+} );
