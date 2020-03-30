13bd0d255bac0896f03ad99caf7ec6916a9f54b1
Stephen Dickinson <stephencottontail@me.com>
2020-03-30 12:16:36 -0600

    Add JSX tags for menu
    Also includes some final styling fixes

--- lib/header.php
+++ lib/header.php
@@ -8,11 +8,17 @@
 	add_action( 'genesis_after_header', function() {
 		echo '<div class="nav-panel">';
 
+		make_jsx_tag_open( 'Nav' );
+		make_jsx_attr( 'type', 'primary' );
+		make_jsx_tag_close();
 		// oops, i forgot to merge in the JSX tags branch
 		genesis_nav_menu( array(
 			'theme_location' => 'primary'
 		) );
 
+		make_jsx_tag_open( 'Nav' );
+		make_jsx_attr( 'type', 'social' );
+		make_jsx_tag_close();
 		genesis_nav_menu( array(
 			'theme_location' => 'social'
 		) );
