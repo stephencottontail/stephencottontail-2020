ec4bcb76f924a6de33507d9c0a51d47ed0337faa
Stephen Dickinson <stephencottontail@me.com>
2020-04-27 16:26:22 -0600

    Overhaul nav styles for a better look

--- lib/header.php
+++ lib/header.php
@@ -8,20 +8,34 @@
 	add_action( 'genesis_after_header', function() {
 		echo '<div class="nav-panel">';
 
-		make_jsx_tag_open( 'Nav' );
-		make_jsx_attr( 'type', 'primary' );
-		make_jsx_tag_close();
-		// oops, i forgot to merge in the JSX tags branch
 		genesis_nav_menu( array(
 			'theme_location' => 'primary'
 		) );
-
-		make_jsx_tag_open( 'Nav' );
-		make_jsx_attr( 'type', 'social' );
-		make_jsx_tag_close();
 		genesis_nav_menu( array(
 			'theme_location' => 'social'
 		) );
 
 		echo '</div><!-- .nav-panel -->';
 	} );
+
+	function add_nav_jsx( $nav_output, $nav, $args ) {
+		ob_start();
+		make_jsx_tag_open( 'Nav' );
+		if ( 'social' == $args['theme_location'] ) {
+			make_jsx_attr( 'type', 'social' );
+		} else {
+			make_jsx_attr( 'type', 'primary' );
+		}
+		make_jsx_tag_close();
+		
+		$jsx = ob_get_clean();
+
+		/**
+		 * This assumes the first `>` in `$nav_output` will be in `<nav>`,
+		 * so let's hope Genesis doesn't change up the nav menu output
+		 */
+		return preg_replace( '/>/', '>' . $jsx, $nav_output, 1 );
+	}
+
+	add_filter( 'genesis_do_nav', 'add_nav_jsx', 10, 3 );
+	add_filter( 'genesis_social_nav', 'add_nav_jsx', 10, 3 );
